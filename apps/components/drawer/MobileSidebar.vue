ea669923bb165722e3789d5553086a775b4e670b
Godwin peter. O <me@godwin.dev>
2024-03-24 18:41:13 +0100

    added initial mobile sidebar

---
+++ apps/components/drawer/MobileSidebar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { Workspace } from '@trace/shared';
+import SidebarList from '@/components/drawer/SidebarList.vue';
+import ThemeSwitcher from '@/components/extra/ThemeSwitcher.vue';
+import type { RouteMenu } from '@trace/base/typings';
+import { computed } from 'vue';
+
+defineOptions({ name: 'MobileSidebar' });
+
+interface IProps {
+  overviewFilter: string[];
+}
+
+defineProps<IProps>();
+const { modelValue, modules, darkMode } = defineModels<{
+  workspace: Workspace;
+  modelValue: boolean;
+  darkMode: boolean;
+  modules: RouteMenu[];
+}>();
+const genralModuleMenu = computed(() => []);
+</script>
+
+<template>
+  <q-drawer v-model="modelValue" show-if-above side="left">
+    <q-scroll-area class="fit fixed-bottom">
+      <sidebar-list :items="genralModuleMenu" />
+    </q-scroll-area>
+    <div class="fixed-bottom full-width column items-center q-mb-sm">
+      <theme-switcher v-model="darkMode" />
+    </div>
+  </q-drawer>
+</template>
